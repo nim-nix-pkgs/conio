fba18bc29c7b7151153d7f23c0c484aa8ad741f7
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:42 -0300

    chore: re-index ../nimpkgs/c/conio/master

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-conio-master.flake = false;
   inputs.src-conio-master.owner = "guevara-chan";
-  inputs.src-conio-master.ref   = "refs/heads/master";
+  inputs.src-conio-master.ref   = "master";
   inputs.src-conio-master.repo  = "conio";
   inputs.src-conio-master.type  = "github";
   
